5d968eba5a3012ddf8e6ae8b65cfc9c20f4702e1
unknown <noruzi206@gmail.com>
2023-02-14 23:13:07 +0330

    add validation code in Javascript

--- javascript.js
+++ javascript.js
@@ -1,3 +1,4 @@
+//changing mode dark or light
 const btnmode=document.querySelector('.btn-mode');
 const mode=document.querySelector('.mode');
 const body=document.querySelector('.body');
@@ -17,3 +18,55 @@ function ChangeMode(){
     loginbtn.classList.toggle('active');
 };
 
+
+//validation
+const usernameInput=document.querySelector(".user-input");
+const passwordInput=document.querySelector(".pass-input");
+const usernameMsg=document.querySelector(".username-msg");
+const passwordMsg=document.querySelector(".password-msg");
+const signinMsg=document.querySelector(".signin-msg");
+const signinBtn=document.querySelector(".login-button");
+
+signinBtn.addEventListener("click", signIn);
+function signIn(event) {
+   event.preventDefault();
+   usernameMsg.innerText="";
+   passwordMsg.innerText="";
+   const usernameValue= usernameInput.value;
+   const passwordValue= passwordInput.value;
+   let ifSendData=true;
+
+   if(usernameValue.length===0 || usernameValue.indexOf("@")===-1 || usernameValue.indexOf(".")===-1){
+      usernameMsg.innerText="Please enter a valid email";
+      ifSendData=false;
+   }
+
+   if(passwordValue.length===0){
+      passwordMsg.innerText="Please enter your password"
+      ifSendData=false;
+   }else if (passwordValue.length <= 5){
+      passwordMsg.innerText="Your password is too short";
+      ifSendData=false;
+   }
+
+   if(ifSendData){
+      const body = JSON.stringify({
+         username: usernameValue,
+         password: passwordValue,
+      })
+      const headers={
+         "content-Type": "application/json; charset=UTF-8"
+      }
+      fetch('https://jsonplaceholder.typicode.com/posts',{
+         method: "POST",
+         body: body,
+         headers: headers
+      })
+
+      .then(response => {
+         if(response.ok){
+            signinMsg.innerText="You sign in successfully"
+         }
+      })
+   }
+}
